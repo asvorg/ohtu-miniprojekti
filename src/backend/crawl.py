05388ff52b45ddce15bbd4eca4e13c770bc237a3
Aapo Koskinen <aapo_koskinen@hotmail.com>
2023-11-15 14:31:01 +0200

    crawl_acm

--- src/backend/crawl.py
+++ src/backend/crawl.py
@@ -8,7 +8,11 @@ def crawl_acm(url):
     #keep only the first paragraph
     abstract = abstract[abstract.find('<p>')+3:abstract.find('</p>')]
     #get references
-    references = text[text.find('<div class="references">'):]
+    references = text[text.find('<ol class="rlist references__list references__numeric">'):]
+    #get all span class= "references__note"
+    references = references.split('<span class="references__note">')
+    for i in references:
+        print(i)
 
     return references
 
