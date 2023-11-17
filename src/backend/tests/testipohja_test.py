8cb09bc901ffdacbb248120e041cdcb85f26a4f9
kaa <113045348+kaahy@users.noreply.github.com>
2023-11-17 13:38:59 +0200

    Testien aloitustoimia

---
+++ src/backend/tests/testipohja_test.py
@@ -0,0 +1,13 @@
+import unittest
+from crawl import crawl_acm
+
+
+class TestCrawAcm(unittest.TestCase):
+    def setUp(self):
+        pass
+        #print("Set up goes here")
+
+    def test_hello_world(self):
+        pass
+        #tulos = crawl_acm("https://dl.acm.org/doi/10.1145/2380552.2380613")
+        #self.assertEqual("Hello world", "Hello world2")
