e1b30e6e43122ddb932377c1e477da57aefcc8b8
Aapo Koskinen <aapo_koskinen@hotmail.com>
2023-11-17 14:49:08 +0200

    testejä

---
+++ src/backend/article_func.py
@@ -0,0 +1,72 @@
+import datetime
+
+
+def read_user_input_article(author, title, journal, year, volume=0, number=0, pages=0, month=0, note=""):
+    '''Read user input and return a tuple of the paper's information'''
+    if not author or not title or not journal or not year:
+        raise ValueError("Author, title, journal and year are required fields")
+    year = int(year)
+    volume = int(volume)
+    number = int(number)
+    pages = int(pages)
+    if year < 0 or year > datetime.datetime.now().year:
+        raise ValueError("Invalid year")
+    author = author.title()
+    title = title.title()
+    journal = journal.title()
+    author = author.strip()
+    title = title.strip()
+    journal = journal.strip()
+
+    return author, title, journal, year, volume, number, pages, month, note
+
+def generate_cite_key(author, year):
+    '''Generate a cite key for a paper'''
+    author = author.lower()
+    return author.split(" ")[-1] + ":" + str(year)
+
+
+def to_bibtex_article(author, title, journal, year, volume=0, number=0, pages=0, month=0, note=""): #ei valmis, pitää handlata tyhjät kentät
+    '''Convert a paper's information to bibtex format'''
+    cite_key = generate_cite_key(author, year)
+    res = "@article{" + cite_key + ",\n"
+    input_str = read_user_input_article(author, title, journal, year, volume, number, pages, month, note)
+    #add to bibtext, handle empty fields with match case
+    try:
+        for i, value in enumerate(input_str):
+            if i == 0 and value:
+                res += f" author = {{{value}}},\n"
+            elif i == 1 and value:
+                res += f" title = {{{value}}},\n"
+            elif i == 2 and value:
+                res += f" journal = {{{value}}},\n"
+            elif i == 3 and value:
+                res += f" year = {{{value}}},\n"
+            elif i == 4 and value:
+                res += f" volume = {{{value}}},\n"
+            elif i == 5 and value:
+                res += f" number = {{{value}}},\n"
+            elif i == 6 and value:
+                res += f" pages = {{{value}}},\n"
+            elif i == 7 and value:
+                res += f" month = {{{value}}},\n"
+            elif i == 8 and value:
+                res += f" note = {{{value}}},\n"
+
+        res += "}"
+    except NameError:
+        pass
+    return res
+
+author = "Matti Meikäläinen"
+title = "Tämä on otsikko"
+journal = "Journal of Journals"
+year = 2020
+volume = 1
+number = 2
+pages = 3
+month = 4
+note = "Tämä on huomautus"
+
+author, title, journal, year, volume, number, pages, month, note = read_user_input_article(author, title, journal, year, volume, number, pages)
+print(to_bibtex_article(author, title, journal, year, volume, number, pages, month, note))
--- src/backend/tests/testipohja_test.py
+++ src/backend/tests/testipohja_test.py
@@ -1,13 +1,37 @@
 import unittest
 from crawl import crawl_acm
+import article_func
 
 
-class TestCrawAcm(unittest.TestCase):
+#class TestCrawlAcm(unittest.TestCase):
+#    def setUp(self):
+#        pass
+#        #print("Set up goes here")
+
+class TestArticleFunc(unittest.TestCase):
     def setUp(self):
         pass
         #print("Set up goes here")
 
-    def test_hello_world(self):
-        pass
-        #tulos = crawl_acm("https://dl.acm.org/doi/10.1145/2380552.2380613")
-        #self.assertEqual("Hello world", "Hello world2")
+    def test_generate_cite_key(self):
+        
+        author = "Matti Meikäläinen"
+        year = 2020
+        tulos = article_func.generate_cite_key(author, year)
+        self.assertEqual(tulos, "meikäläinen:2020")
+    
+    def test_to_bibtex_article(self):
+        author = "Matti Meikäläinen"
+        title = "Tämä on otsikko"
+        journal = "Journal of Journals"
+        year = 2020
+        volume = 1
+        number = 2
+        pages = 3
+        month = 4
+        note = "Tämä on huomautus"
+        tulos = article_func.to_bibtex_article(author, title, journal, year, volume, number, pages, month, note)
+        self.assertEqual(tulos, "@article{meikäläinen:2020,\n author = {Matti Meikäläinen},\n title = {Tämä on otsikko},\n journal = {Journal of Journals},\n year = {2020},\n volume = {1},\n number = {2},\n pages = {3},\n month = {4},\n note = {Tämä on huomautus},\n}")
+
+    if __name__ == '__main__':
+        unittest.main()
