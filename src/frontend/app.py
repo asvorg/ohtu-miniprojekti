67059391f395df22c3d95b6f567cbdf87a8dfc6e
Anna Essina <anna.essina@helsinki.fi>
2023-11-17 12:31:28 +0200

    etusivun aloitus

--- src/frontend/app.py
+++ src/frontend/app.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from flask import render_template, request
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Heipparallaa!"
+    return render_template("index.html")
+
+@app.route("/result", methods=["POST"])
+def result():
+    return render_template("result.html"), a=request.form["Kirjoittaja"], b=request.form["Otsikko"], c=request.form["Julkaisuvuosi"], d=request.form["Artikkelin nimi"]
