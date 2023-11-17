0a59ec87d530ecc88ecaddebf1bea1875809ea68
Anna Essina <anna.essina@helsinki.fi>
2023-11-17 12:49:47 +0200

    app.py päivitys

--- src/frontend/app.py
+++ src/frontend/app.py
@@ -10,4 +10,4 @@ def index():
 
 @app.route("/result", methods=["POST"])
 def result():
-    return render_template("result.html"), a=request.form["Kirjoittaja"], b=request.form["Otsikko"], c=request.form["Julkaisuvuosi"], d=request.form["Artikkelin nimi"]
+    return render_template("result.html", a=request.form["Kirjoittaja"], b=request.form["Otsikko"], c=request.form["Julkaisuvuosi"], d=request.form["Artikkelin nimi"])
