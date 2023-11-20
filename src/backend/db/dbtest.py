246f0be730f79079e6c14f07d3527ea4a279ff27
Aapo Koskinen <aapo_koskinen@hotmail.com>
2023-11-20 16:25:39 +0200

    added dbconf.txt

--- src/backend/db/dbtest.py
+++ src/backend/db/dbtest.py
@@ -1,27 +1,18 @@
-# pylint: disable=W0611
 from pymongo import MongoClient, errors
 from pymongo.server_api import ServerApi
 
 
-def init_db():
-    """Initialize database connection for the create user function"""
-    client = MongoClient('mongodb+srv://kaariainenroope:<password>@ohtuminiprojekti-bibtex.19oezdh.mongodb.net/?retryWrites=true&w=majority')
-    database = client['data']
-    password_collection = database[password_collection]
-    return password_collection
+def test_connection():
+    uri = "mongodb+srv://kaariainenroope:<password>@ohtuminiprojekti-bibtex.19oezdh.mongodb.net/?retryWrites=true&w=majority"
 
+    # Create a new client and connect to the server
+    client = MongoClient(uri, server_api=ServerApi('1'))
 
-
-#add a article to database
-uri = "mongodb+srv://kaariainenroope:<password>@ohtuminiprojekti-bibtex.19oezdh.mongodb.net/?retryWrites=true&w=majority"
-
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
-
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
-    
+    # Send a ping to confirm a successful connection
+    try:
+        client.admin.command('ping')
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        print(e)
+        
+test_connection()
