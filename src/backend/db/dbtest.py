52edb42d4f4cbeb994ee91d91620e926e4e0fffc
Aapo Koskinen <aapo_koskinen@hotmail.com>
2023-11-22 12:10:25 +0200

    tietokantajuttyi

--- src/backend/db/dbtest.py
+++ src/backend/db/dbtest.py
@@ -15,5 +15,27 @@ def test_connection():
         print("Pinged your deployment. You successfully connected to MongoDB!")
     except Exception as e:
         print(e)
-        
-test_connection()
+
+def add_to_database():
+    uri = "mongodb+srv://testiuser:testisalasana123@ohtuminiprojekti-bibtex.19oezdh.mongodb.net/test?retryWrites=true&w=majority"
+
+    client = MongoClient(uri, server_api=ServerApi('1'))
+    db = client["bibtex"]
+    collection = db["articles"]
+    article = {
+        "title": "testi",
+        "author": "testaaja",
+        "year": 2021,
+        "journal": "testaus",
+        "volume": 1,
+        "number": 1,
+        "pages": "1-2",
+        "month": 1,
+        "note": "testi",
+        "key": "testi"
+    }
+    collection.insert_one(article)
+    if collection.find_one({"title": "testi"}):
+        print("Added to database")
+
+add_to_database()
