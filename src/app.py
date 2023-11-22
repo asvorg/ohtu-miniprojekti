8191b28644c0f2d29e36f4ab324d271018c5615e
Inkeri <inkeri.ahlstrom@helsinki.fi>
2023-11-22 14:53:55 +0200

    Käyttäjän syöte tulostuu BibTeX-muodossa

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template, request
+from backend.article_func import read_user_input_article, to_bibtex_article
 
 
 app = Flask(__name__, template_folder='frontend/templates')
@@ -10,7 +11,18 @@ def index():
 
 @app.route("/result", methods=["POST"])
 def result():
-    return render_template("result.html", author=request.form["Kirjoittaja"], title=request.form["Otsikko"], year=request.form["Julkaisuvuosi"], article_name=request.form["ArtikkelinNimi"])
+    try:
+        author = request.form["Kirjoittaja"]
+        title = request.form["Otsikko"]
+        article_name = request.form["ArtikkelinNimi"]
+        year = request.form["Julkaisuvuosi"]
+
+        bibtex_result = to_bibtex_article(author, title, article_name, year)
+
+        return render_template("result.html", article_name=article_name, bibtex_result=bibtex_result)
+
+    except ValueError as e:
+        return render_template("error.html", error_message=str(e))
 
 @app.route("/list")
 def list():
