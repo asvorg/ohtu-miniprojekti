586725fa4255e0b3c46773d3bd9fb7ff959dac52
Aapo Koskinen <aapo_koskinen@hotmail.com>
2023-11-24 15:36:14 +0200

    pieni refactor

--- src/backend/db/db_func.py
+++ src/backend/db/db_func.py
@@ -5,14 +5,18 @@ from pymongo.server_api import ServerApi
 from backend.article_func import generate_cite_key, read_user_input_article, to_bibtex_article
 
 
-
-def add_article_to_db(user, article):
-    '''Add an article to the database'''
+def connect_to_db():
+    '''Connect to the database'''
     uri = "mongodb+srv://testiuser:testisalasana123@ohtuminiprojekti-bibtex.19oezdh.mongodb.net/test?retryWrites=true&w=majority"
 
     client = MongoClient(uri, server_api=ServerApi('1'))
     db = client["bibtex"]
     collection = db["articles"]
+    return collection, db, client, uri
+
+def add_article_to_db(user, article):
+    '''Add an article to the database'''
+    collection, db, client = connect_to_db()
     article_dict = splice_article(article)
     cite_key = generate_cite_key(article_dict["author"], article_dict["year"])
     #add to database
@@ -39,53 +43,41 @@ def splice_article(article): #article in bibtex format
 
 def get_article_from_db_by_user(user):
     '''Get all articles from the database by user'''
-    uri = "mongodb+srv://testiuser:testisalasana123@ohtuminiprojekti-bibtex.19oezdh.mongodb.net/test?retryWrites=true&w=majority"
-
-    client = MongoClient(uri, server_api=ServerApi('1'))
-    db = client["bibtex"]
-    collection = db["articles"]
+    collection, db, client,uri = connect_to_db()
     for article in collection.find({"user":user}):
         return article
 
 def get_article_from_db_by_cite_key(user, cite_key):
     '''Get an article from the database by cite key'''
-    uri = "mongodb+srv://testiuser:testisalasana123@ohtuminiprojekti-bibtex.19oezdh.mongodb.net/test?retryWrites=true&w=majority"
-
-    client = MongoClient(uri, server_api=ServerApi('1'))
-    db = client["bibtex"]
-    collection = db["articles"]
+    collection, db, client,uri = connect_to_db()
     for article in collection.find({"user": user, "cite_key": cite_key}):
         return article
     
 def get_article_from_db_by_author(user, author):
     '''Get an article from the database by author'''
-    uri = "mongodb+srv://testiuser:testisalasana123@ohtuminiprojekti-bibtex.19oezdh.mongodb.net/test?retryWrites=true&w=majority"
-
-    client = MongoClient(uri, server_api=ServerApi('1'))
-    db = client["bibtex"]
-    collection = db["articles"]
+    collection, db, client,uri = connect_to_db()
     for article in collection.find({"user": user, "author": author}):
         return article
 
 def get_article_from_db_by_year(user, year):
     '''Get an article from the database by year'''
-    uri = "mongodb+srv://testiuser:testisalasana123@ohtuminiprojekti-bibtex.19oezdh.mongodb.net/test?retryWrites=true&w=majority"
-
-    client = MongoClient(uri, server_api=ServerApi('1'))
-    db = client["bibtex"]
-    collection = db["articles"]
+    collection, db, client,uri = connect_to_db()
     for article in collection.find({"user": user, "year": year}):
         return article
 
 def delete_article_by_cite_key(user,cite_key):
     '''Delete an article from the database'''
-    uri = "mongodb+srv://testiuser:testisalasana123@ohtuminiprojekti-bibtex.19oezdh.mongodb.net/test?retryWrites=true&w=majority"
-
-    client = MongoClient(uri, server_api=ServerApi('1'))
-    db = client["bibtex"]
-    collection = db["articles"]
+    collection, db, client,uri = connect_to_db()
     
     collection.delete_one({"user": user, "cite_key": cite_key})
+
+def edit_article_by_cite_key(user, cite_key, article): #ei mitää hajua toimiiko tää
+    '''Edit an article in the database'''
+    old_article = get_article_from_db_by_cite_key(user, cite_key)
+    for key in article:
+        if article[key]:
+            old_article[key] = article[key]
+    
 
 #author = "Matti Meikäläinen"
 #title = "Tämä on otsikko2"
