ce8c970e2df27ef38448e89f2a8345b006b141ab
Aapo Koskinen <aapo_koskinen@hotmail.com>
2023-11-28 16:33:24 +0200

    crawl.py edistyy

--- src/backend/crawl.py
+++ src/backend/crawl.py
@@ -1,22 +1,38 @@
-# pylint: skip-file
 import requests
+from datetime import datetime
 
 
 def crawl_acm(url):
-    '''''''''''
-    Crawl the abstract and references of a paper from ACM Digital Library
-    '''''''''''
-    #get the html text
+    '''Crawl a paper from ACM'''
     text = requests.get(url).text
-    #keep only the text in div with class 'abstractSection abstractInFull'
-    abstract = text[text.find('<div class="abstractSection abstractInFull">'):]
-    #keep only the first paragraph
-    abstract = abstract[abstract.find('<p>')+3:abstract.find('</p>')]
-    #get references
-    references = text[text.find('<ol class="rlist references__list references__numeric">'):]
-    #get all span class= "references__note"
-    references = references.split('<span class="references__note">')
+    title = text.split("<title>")[1].split("</title>")[0]
+    abstract = text.split("<div class=\"abstractSection abstractInFull\">")[1].split("</div>")[0]
+    #clean the abstract
+    abstract = abstract.replace("<p>", "")
+    abstract = abstract.replace("</p>", "")
+    abstract = abstract.replace("\n","")
+    abstract = abstract.replace("\t","")
 
-    return references
+    authors = text.split("Authors:")[1].split("/></a></div>")[0]
+    authors = authors.split("aria-hidden=\"true\"/>")[1:]
+    
+    for i in range(len(authors)):
+        authors[i] = authors[i].split("</span>")[0]
+        authors[i] = authors[i].strip()
+    
+    published = text.split("<span class=\"CitationCoverDate\">")[1].split("</span>")[0]
+
+    pages = text.split("<span class=\"epub-section__pagerange\">")[1].split("</span>")[0]
+    pages = pages.replace("Pages ", "")
+    pages = pages.strip()
+    
+    return title, abstract, authors, published, pages
 
 print(crawl_acm("https://dl.acm.org/doi/10.1145/2380552.2380613"))
+
+
+#get references as a list
+#references = text.split("<li class=\"references__item\">")
+#split refnces by line
+#for i in range(len(references)):
+#    references[i] = references[i].split("\n")
