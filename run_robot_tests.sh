171de2856a27a946f0d921b628e6dc685a43affe
Roope <kaariainenroope@gmail.com>
2023-12-01 12:21:44 +0200

    run_robot...

--- run_robot_tests.sh
+++ run_robot_tests.sh
@@ -6,7 +6,7 @@ poetry run flask run &
 
 echo "Checking server status..."
 # odetetaan, että palvelin on valmiina ottamaan vastaan pyyntöjä
-while [[ "$(curl -s -o /dev/null -w '%{http_code}' localhost:5000/ping)" != "200" ]];
+while [[ "$(curl -s -o /dev/null -w '%{http_code}' localhost:5000/)" != "200" ]];
   do sleep 1;
 done
 
